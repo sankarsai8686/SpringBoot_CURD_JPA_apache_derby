938c7210b8d08847d6427a2106e6576ad23207aa
SANKAR <SANKAR@DESKTOP-QS6VIUV>
2018-08-25 10:51:41 +0530

    Main Class updated.

--- SpringBootCURD_REST_JPA/src/main/java/com/SpringBoot/SpringBootCurdRestJpaApplication.java
+++ SpringBootCURD_REST_JPA/src/main/java/com/SpringBoot/SpringBootCurdRestJpaApplication.java
@@ -2,6 +2,8 @@ package com.SpringBoot;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,30 +13,37 @@ import org.springframework.context.annotation.Configuration;
 import com.SpringBoot.Entities.Ticket;
 import com.SpringBoot.Services.TicketBookingServices;
 
-@Configuration
+/*@Configuration*/
 @SpringBootApplication
-@ComponentScan(basePackages ={"com.SpringBoot","com.SpringBoot.Services"})
-public class SpringBootCurdRestJpaApplication {
+/*@ComponentScan(basePackages ={"com.SpringBoot","com.SpringBoot.Services"})*/
+public class SpringBootCurdRestJpaApplication implements CommandLineRunner
+{
 
+	@Autowired
+	private TicketBookingServices ticketBookingServices;
+	
 	public static void main(String[] args) {
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootCurdRestJpaApplication.class, args);
+		/*ConfigurableApplicationContext applicationContext =*/ 
+		
+		SpringApplication.run(SpringBootCurdRestJpaApplication.class, args);
 	
 	
 	/*	Exception in thread "main" org.springframework.beans.factory.NoSuchBeanDefinitionException: No bean named
 		check Service class name ticketBookingService I changed ticketBookingServices*/
 		
-		TicketBookingServices ticketBookingService = applicationContext.getBean("ticketBookingServices",TicketBookingServices.class);
+		/*TicketBookingServices ticketBookingService = applicationContext.getBean("ticketBookingServices",TicketBookingServices.class);*/
+		
+
+	}
+	@Override
+	public void run(String... arg0) throws Exception {
 		Ticket ticket = new Ticket();
 		ticket.setBookingDate(new Date());
 		ticket.setPassengerName("sai");
 		ticket.setDestination("pune");
 		ticket.setSourceStation("renigunta");
 		ticket.setEmail("sai@gmail.com");
-		ticketBookingService.createTicket(ticket);
-	
-	
-	
-	
-	
+		ticketBookingServices.createTicket(ticket);
+		
 	}
 }
